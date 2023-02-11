3454158172d967fca18b0ee4ba0d63341bffeb29
selhuberb <selhuberb@mediamarktsaturn.com>
2023-02-11 14:30:17 +0100

    chore: update modules and removal of not needed scripts

--- scripts/be-node-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "Install bash and execute 'wait-for-it.sh' script"
-apk add --update bash
-./scripts/wait-for-it.sh $DB_HOST:3306 --timeout=30 --strict -- echo "mariadb up and running"
-
-npm run migration:run
-npm run seed:run
-npm run dev
